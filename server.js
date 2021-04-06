c75829a6fe5910bcb6127fad495aa562f010b7ce
arek <areksa123@o2.pl>
2021-04-06 14:29:11 +0200

    d

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import session from 'express-session';
 import * as path from 'path';
-import Database from './modules/my_database_methods';
-// import Database from './modules/my_database_methods'
+import Database from './modules/my_database_methods.js';
+
 const __dirname = path.resolve();
 const app = express();
 
@@ -19,11 +19,14 @@ app.use(session({
     resave: false,
 }));
 async function wyswietl_sesje() {
-    console.log(await Database.methods.readAllFrom(sesje))
+    return await Database.methods.readAllFrom(sesje)
 }
 app.get('/', function (req, res) {
     // console.log(req.session, req.sessionID)
-    wyswietl_sesje();
+    res.set({ 'Content-Type': 'application/json' })
+    res.send(wyswietl_sesje())
+
+    //wyswietl_sesje();
 }).listen(PORT, function () {
     console.log("PORT " + PORT + " włączony")
 })
