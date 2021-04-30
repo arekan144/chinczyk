6fa73520edf70a42523c42cf716f09c29fd26f85
arek <areksa123@o2.pl>
2021-04-30 18:04:30 +0200

    Ostatni(chyyba)

--- src/addres.js
+++ src/addres.js
@@ -1,6 +1,6 @@
 export default new class address {
     constructor() {
-        this.adres = "https://her-testus-destus.herokuapp.com/";
+        this.adres = "https://chinczyk-gra-arkadiuszsala.herokuapp.com/";
         //this.adres = "http://localhost:4000/";
     }
 }
